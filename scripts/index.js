9429e4347d6b17966b63830257c3d1ef46931b7f
Megan Elizabeth <manthony0322@gmail.com>
2024-07-18 01:38:49 -0500

    refactor (gallery modal)/ restructure event listeners, update CSS classes

--- scripts/index.js
+++ scripts/index.js
@@ -26,39 +26,50 @@ const initialCards = [
 ];
 
 /* Elements */
-const profileEditButton = document.querySelector("#profile-edit-button");
+//Wrapppers
 const profileEditModal = document.querySelector("#profile-edit-modal");
-const profileEditClose = document.querySelector("#profile-edit-close");
+const galleryEditModal = document.querySelector("#gallery-add-modal");
+const galleryEl = document.querySelector("#gallery-cards");
+const galleryCardTemplate = document.querySelector("#gallery-card-template")
+  .content.firstElementChild;
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const galleryEditForm = galleryEditModal.querySelector(".modal__form");
+
+// Buttons and DOM Nodes
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileModalCloseButton = profileEditModal.querySelector(".modal__close");
+const galleryAddButton = document.querySelector("#gallery-add-button");
+const galleryModalCloseButton = galleryEditModal.querySelector(".modal__close");
 const profileTitle = document.querySelector(".profile__title");
 const profileDescription = document.querySelector(".profile__description");
+
+//Form data
 const profileTitleInput = document.querySelector("#profile-title-input");
 const profileDescriptionInput = document.querySelector(
   "#profile-description-input"
 );
-const profileEditForm = profileEditModal.querySelector(".modal__form");
-const galleryEl = document.querySelector("#gallery-cards");
-const galleryCardTemplate = document.querySelector("#gallery-card-template")
-  .content.firstElementChild;
 
 /* Functions */
-function closePopup() {
-  profileEditModal.classList.remove("modal_opened");
+function openModal(modal) {
+  modal.classList.add("modal_opened");
+}
+function closeModal(modal) {
+  modal.classList.remove("modal_opened");
 }
 
 function getCardElement(galleryCardData) {
-  // clone the template element with all its content and store it in a `cardElement` variable
   const galleryCardElement = galleryCardTemplate.cloneNode(true);
-  // access the card title and image and store them in variables
+
   const galleryCardImageEl =
     galleryCardElement.querySelector(".gallery__image");
   const galleryCardTextEl = galleryCardElement.querySelector(".gallery__text");
-  // set the path to the image to the `link` field of the object
+
   galleryCardImageEl.src = galleryCardData.link;
-  // set the image alt text to the `name` field of the object
+
   galleryCardImageEl.alt = galleryCardData.name;
-  // set the card title to the `name` field of the object, too
+
   galleryCardTextEl.textContent = galleryCardData.name;
-  // return the ready HTML element with the filled-in data
+
   return galleryCardElement;
 }
 
@@ -70,23 +81,23 @@ function handleProfileEditSubmit(evt) {
   closePopup();
 }
 
-/* Event Listeners */
-//* ! 2/7 create template for adding a card?
-//* !2.1 clone modal for name? and add to it per Figma - HTML, CSS & JS
-
-//* !2.2 eventlistener 'click' to open, and 2.3 to close
-
 profileEditButton.addEventListener("click", () => {
   profileTitleInput.value = profileTitle.textContent;
   profileDescriptionInput.value = profileDescription.textContent;
-  profileEditModal.classList.add("modal_opened");
+  openModal(profileEditModal);
 });
-
-profileEditClose.addEventListener("click", closePopup);
-
+profileModalCloseButton.addEventListener("click", () =>
+  closeModal(profileEditModal)
+);
 profileEditForm.addEventListener("submit", handleProfileEditSubmit);
 
-//* ! 1/7 rewrite the for loop with a forEach loop - already done?
+// Add new card button
+galleryAddButton.addEventListener("click", () => openModal(galleryEditModal));
+galleryModalCloseButton.addEventListener("click", () =>
+  closeModal(galleryEditModal)
+);
+galleryEditForm.addEventListener("submit", handleProfileEditSubmit);
+
 initialCards.forEach((galleryCardData) => {
   const galleryCardElement = getCardElement(galleryCardData);
   galleryEl.append(galleryCardElement);
